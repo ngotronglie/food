15ffbab52adbc42769ff5865b2baed0e2e5aecf5
Trong Liem <ngotrongliem2004@gmail.com>
2023-03-16 20:59:29 +0700

    add baitap

---
+++ main.js
@@ -0,0 +1,35 @@
+const myInfo = {
+    myName: "liem",
+    phone: "08943573",
+    mail: "kshdf@gmail.com",
+    sever: "trai dat"
+
+}
+const minh = {
+    myName: "minh",
+    phone: "08943573",
+    mail: "kshdf@gmail.com",
+    sever: "trai dat"
+
+}
+const hau = {
+    myName: "hau",
+    phone: "08943573",
+    mail: "kshdf@gmail.com",
+    sever: "trai dat"
+
+}
+
+const myStudent = []
+
+myStudent.push(myInfo)
+myStudent.push(minh)
+myStudent.push(hau)
+console.log(myStudent)
+
+for (let i = 0; i < myStudent.length; i++) {
+    console.log(myStudent[i])
+}
+
+
+ 
